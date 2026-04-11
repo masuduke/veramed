f13626cd8b71d4d7c389d31359e17f3bade7ea75
Masud Rana <masud.uk.e@gmail.com>
2026-04-11 20:16:05 +0100

    Fix admin auth token 401 errors

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+﻿import axios from 'axios';
 import { useAuthStore } from './auth-store';
 
 export const api = axios.create({
@@ -12,3 +12,15 @@ api.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = 'Bearer ' + token;
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      const { logout } = useAuthStore.getState();
+      await logout();
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
